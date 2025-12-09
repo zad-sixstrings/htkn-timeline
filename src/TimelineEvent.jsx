9315830046d09ce16d9af7ab3358a98f286727c0
Zad Sixstrings <toxxiczad@gmail.com>
2025-12-09 13:31:08 +0100

    added lightbox feature

--- src/TimelineEvent.jsx
+++ src/TimelineEvent.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { EVENT_ICONS } from "./utils/constants";
 import timelineMarker from "./assets/timeline-marker.png";
 import Parser from "./Parser";
-import "./tooltips.css"
+import "./lightbox.css";
+import "./tooltips.css";
 
 // Display date based on original precision
 function formatDate(dateStr) {
@@ -22,42 +24,63 @@ function formatDate(dateStr) {
 }
 
 export default function TimelineEvent({ event, isNew }) {
+  const [lightboxOpen, setLightboxOpen] = useState(false);
+
   return (
-    <div className={`timeline-event-container ${isNew ? "fade-in" : ""}`}>
-      <div className="timeline-event right">
-        {event.category && EVENT_ICONS[event.category] && (
+    <>
+      {lightboxOpen && (
+        <div
+          className="lightbox-backdrop"
+          onClick={() => setLightboxOpen(false)}
+        >
+          <p>Cliquer en dehors de l'image pour fermer.</p>
           <img
-            src={`/event-icons/${EVENT_ICONS[event.category]}`}
-            alt={event.category}
-            className="event-icon"
+            src={`/images/${event.image}`}
+            alt={event.title}
+            className="lightbox-image"
+            onClick={(e) => e.stopPropagation()}
           />
-        )}
-        <div className="timeline-event-date">
-          <p>{formatDate(event.date)}</p>
         </div>
-        <div className="timeline-event-title">
-          <h3>{event.title}</h3>
-        </div>
-        <div className="timeline-event-content">
-          <Parser text={event.description} />
-        </div>
-        {event.image && (
-          <div className="timeline-event-image">
+      )}
+      <div className={`timeline-event-container ${isNew ? "fade-in" : ""}`}>
+        <div className="timeline-event right">
+          {event.category && EVENT_ICONS[event.category] && (
             <img
-              src={`/images/${event.image}`}
-              alt={event.title}
-              className="event-image"
+              src={`/event-icons/${EVENT_ICONS[event.category]}`}
+              alt={event.category}
+              className="event-icon"
             />
+          )}
+          <div className="timeline-event-date">
+            <p>{formatDate(event.date)}</p>
           </div>
-        )}
+          <div className="timeline-event-title">
+            <h3>{event.title}</h3>
+          </div>
+          <div className="timeline-event-content">
+            <Parser text={event.description} />
+          </div>
+          {event.image && (
+            <div
+              className="timeline-event-image"
+              onClick={() => setLightboxOpen(true)}
+            >
+              <img
+                src={`/images/${event.image}`}
+                alt={event.title}
+                className="event-image"
+              />
+            </div>
+          )}
+        </div>
+        <div className="timeline-marker">
+          <img
+            src={timelineMarker}
+            alt="Marqueur de timeline"
+            className="timeline-marker-logo"
+          />
+        </div>
       </div>
-      <div className="timeline-marker">
-        <img
-          src={timelineMarker}
-          alt="Marqueur de timeline"
-          className="timeline-marker-logo"
-        />
-      </div>
-    </div>
+    </>
   );
 }
